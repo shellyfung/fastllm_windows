f42a79e79c2512f460847bf231fb4c5edc887f6a
shellyfung <shellyfung2013@gmail.com>
2023-05-19 23:22:03 +0800

    fix TCHAR to LPWSTR

--- src/main.cpp
+++ src/main.cpp
@@ -35,12 +35,12 @@ std::string UTF8ToGBK(const std::string& strUTF8)
 	int len = MultiByteToWideChar(CP_UTF8, 0, strUTF8.c_str(), -1, NULL, 0);
 	TCHAR* wszGBK = new TCHAR[len + 1];
 	memset(wszGBK, 0, len * 2 + 2);
-	MultiByteToWideChar(CP_UTF8, 0, strUTF8.c_str(), -1, wszGBK, len);
+	MultiByteToWideChar(CP_UTF8, 0, strUTF8.c_str(), -1, (LPWSTR)wszGBK, len);
 
-	len = WideCharToMultiByte(CP_ACP, 0, wszGBK, -1, NULL, 0, NULL, NULL);
+	len = WideCharToMultiByte(CP_ACP, 0, (LPWSTR)wszGBK, -1, NULL, 0, NULL, NULL);
 	char* szGBK = new char[len + 1];
 	memset(szGBK, 0, len + 1);
-	WideCharToMultiByte(CP_ACP, 0, wszGBK, -1, szGBK, len, NULL, NULL);
+	WideCharToMultiByte(CP_ACP, 0, (LPWSTR)wszGBK, -1, szGBK, len, NULL, NULL);
 	std::string strTemp(szGBK);
 	delete[]szGBK;
 	delete[]wszGBK;
